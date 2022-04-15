5d7c9ec95c8f71306621c9c76f1387322e222c0b
Maxence Machu <me@maxence.space>
2022-04-15 16:20:58 +0200

    Update fizz.test.js

--- tests/fizz.test.js
+++ tests/fizz.test.js
@@ -10,7 +10,7 @@ describe('FizzBuzz', () => {
         if(i % 5 === 0 && i % 3 === 0){
           expect(fizzbuzz(i)).toBe('FizzBuzz');
         } else if(i % 3 === 0){
-          expect(fizzbuzz(i)).toBe('Fizzz');
+          expect(fizzbuzz(i)).toBe('Fizz');
         } else if(i % 5 === 0){
           expect(fizzbuzz(i)).toBe('Buzz');
         } else {
